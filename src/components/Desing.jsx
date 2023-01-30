3fd000d931c9250701e3a474e27e1e9b08393f2b
milidziuban <milagrosdziuban@hotmail.com>
2023-01-30 11:43:49 -0300

    hover

--- src/components/Desing.jsx
+++ src/components/Desing.jsx
@@ -3,6 +3,7 @@ import './Desing.css'
 import WebDesing from './imagenes/webdesing.jpg'
 import planA from './imagenes/planA.jpg'
 import tuenti from './imagenes/tuenti.jpg'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default function Desing() {
     return (
@@ -12,24 +13,30 @@ export default function Desing() {
                 <div className="proyects">
 
                     <div>
-                        <a className="imgA" href="https://www.behance.net/gallery/151858757/Landing-page-Web-Design"> Web Desing
-                            <p> Proyecto realizado con un equipo de Desarrolladores, Diseñadores y Marketing Digital  para un estudio de abogados </p>
+                        <a className="imgA" href="https://www.behance.net/gallery/151858757/Landing-page-Web-Design">
+                            <p className="parrafo">Web Desing: Proyecto realizado con un equipo de Desarrolladores y Diseñadores para un estudio de abogados
+                                <button> VER MÁS <NavigateNextIcon /> </button>
+                            </p>
+                            <img className="img1" src={WebDesing} width='280px' height='280px' alt="webDesing"></img>
                         </a>
-                        <img className="img1" src={WebDesing} width='280px' height='280px' alt="webDesing"></img>
                     </div>
 
                     <div>
-                        <a className="imgA" href="https://www.behance.net/gallery/146600553/Plan-A-Proyecto"> Plan A diseño
-                            <p> Proyecto realizado para curso básico de diseño UX/UI </p>
+                        <a className="imgA" href="https://www.behance.net/gallery/146600553/Plan-A-Proyecto">
+                            <p className="parrafo">Plan A: Diseño de aplicación para movil, proyecto realizado para curso básico de diseño UX/UI
+                                <button> VER MÁS <NavigateNextIcon /> </button>
+                            </p>
+                            <img className="img1" src={planA} width='280px' height='280px' alt="planA"></img>
                         </a>
-                        <img className="img1" src={planA} width='280px' height='280px' alt="planA"></img>
                     </div>
 
                     <div>
-                        <a className="imgA" href="https://www.behance.net/gallery/155386987/Rediseno-de-Tuenti"> Tuenti Rediseño
-                            <p> Proyecto realizado para curso avanzado de diseño UX/UI </p>
+                        <a className="imgA" href="https://www.behance.net/gallery/155386987/Rediseno-de-Tuenti">
+                            <p className="parrafo">Tuenti telefonía: Rediseño de aplicación movil, proyecto realizado para curso avanzado de diseño UX/UI
+                                <button> VER MÁS <NavigateNextIcon /> </button>
+                            </p>
+                            <img className="img1" src={tuenti} width='280px' height='280px' alt="tuenti"></img>
                         </a>
-                        <img className="img1" src={tuenti} width='280px' height='280px' alt="tuenti"></img>
                     </div>
                 </div>
             </div>
@@ -42,7 +49,7 @@ export default function Desing() {
                     src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FVUHEaYh32t48xj0JnonnZI%2FWireframeDziuban%3Fnode-id%3D57%253A1056%26scaling%3Dscale-down%26page-id%3D57%253A975%26starting-point-node-id%3D57%253A1065%26show-proto-sidebar%3D1" allowfullscreen>
                 </iframe>
             </div>
-            <a href="https://www.behance.net/milagrosdziuban1"> Ver más proyectos </a>
+            <a className="more" href="https://www.behance.net/milagrosdziuban1"> Ver más proyectos <NavigateNextIcon /> </a>
         </div>
     )
 }
--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './Developer.css';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
 
 export default function Developer() {
     return (
@@ -8,20 +10,26 @@ export default function Developer() {
                 <h3> FULL STACK </h3>
                 <div className="webproyect">
                     <div>
-                        <a className="imgA" href="https://oasis-library.vercel.app/"> Oasis Library
-                            <p> Proyecto realizado con un equipo de Desarrolladores para el cursado de la carrera de Full Stack en SoyHenry </p>
+                        <a className="imgA" href="https://oasis-library.vercel.app/">
+                            <p className="parrafo">Oasis Library: Proyecto realizado con un equipo de Desarrolladores para el cursado de la carrera de Full Stack en SoyHenry
+                                <button> VER MÁS <NavigateNextIcon /> </button>
+                            </p>
                         </a>
                         <img className="img1" src='https://media4.giphy.com/media/yVGNMRvAYKx75qoMJu/giphy.gif?cid=790b7611560b96d7d7ec623d19fba2daa40a50fdcf3b596f&rid=giphy.gif&ct=g' alt="oasislibrary"></img>
                     </div>
                     <div>
-                        <a className="imgA" href="https://youtu.be/xz8T4tAWKPA"> Proyecto Dogs
-                            <p> Proyecto realizado de manera individual para el cursado de la carrera de Full Stack en SoyHenry </p>
+                        <a className="imgA" href="https://youtu.be/xz8T4tAWKPA"> 
+                            <p className="parrafo">Henry Dogs: Proyecto realizado de manera individual durante el cursado de la carrera de Full Stack en SoyHenry
+                                <button> VER MÁS <NavigateNextIcon /> </button>
+                            </p>
                         </a>
                         <img className="img1" src='https://media4.giphy.com/media/XNGtip6WfBqFBI7ZWi/giphy.gif?cid=790b7611046b25f422c19944f7ab2cd6daa8ff8c796ec557&rid=giphy.gif&ct=g' width='280px' height='280px' alt="webDesing"></img>
                     </div>
                     <div>
-                        <a className="imgA" href="https://oasis-library.vercel.app/"> Proyecto Rick and Morty
-                            <p> Proyecto realizado de forma autonoma para comprender y afinazar conceptos PROXIMAMENTE </p>
+                        <a className="imgA" href="https://oasis-library.vercel.app/"> 
+                            <p className="parrafo">Rick and Morty: Proyecto realizado de manera autónoma para descubrir, comprender y afinazar conceptos.
+                                <button> EN PROCESO </button>
+                            </p>
                         </a>
                         <img className="img1" src='https://media4.giphy.com/media/cximO229fNXDTe0miu/giphy.gif?cid=790b7611184fd00a31a510ccc63636bd081dbac02013e3cb&rid=giphy.gif&ct=g' width='280px' height='280px' alt="webDesing"></img>
                     </div>
